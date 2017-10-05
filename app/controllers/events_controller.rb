6554f594093a0ac14e5f50e1793c052157372e55
Kishan Hulkodu Sheshagiri <kishan.sheshagiri@tamu.edu>
2017-10-04 19:53:23 -0500

    Query to get selected fields
    - By default, the 'events' table contains many data which need not
      be shown in the list page.
    - This commit replaces the default 'Event.all' with a custom query
      to return the specific fields to be shown in the home page.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,7 +4,7 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all
+    @events = Event.getRecords
   end
 
   # GET /events/1
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,16 @@ class Event < ApplicationRecord
   belongs_to :episode
   has_many :locations
   has_many :fatalities
+
+  def self.getRecords
+    find_by_sql("SELECT e.id as id, storm_type, c.name as county_name,
+                   s.name as state_name, begin_time, source
+                 FROM events e
+                 INNER JOIN us_states s
+                   ON e.state_fips = s.id
+                 INNER JOIN counties c
+                   ON e.county_fips = c.fips AND e.state_fips = c.us_state_id
+                 LIMIT 10
+               ")
+  end
 end
