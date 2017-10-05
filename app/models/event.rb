1ce38e42ef692dfaa5ffa8d58a1e1f023f237a15
Kishan Hulkodu Sheshagiri <kishan.sheshagiri@tamu.edu>
2017-10-04 23:47:36 -0500

    Introduce new query for 'find'
    - The default query doesn't return the required elements for the
      edit/show pages of the table.
    - Added a custom SQL query to get additional details from
      the MySQL database.

--- app/models/event.rb
+++ app/models/event.rb
@@ -4,14 +4,46 @@ class Event < ApplicationRecord
   has_many :fatalities
 
   def self.getRecords
-    find_by_sql("SELECT e.id as id, storm_type, c.name as county_name,
-                   s.name as state_name, begin_time, source
-                 FROM events e
-                 INNER JOIN us_states s
-                   ON e.state_fips = s.id
-                 INNER JOIN counties c
-                   ON e.county_fips = c.fips AND e.state_fips = c.us_state_id
-                 LIMIT 10
-               ")
+    find_by_sql("SELECT e.id as id, storm_type,
+          c.name as county_name, s.name as state_name,
+          begin_time, source
+        FROM events e
+        INNER JOIN us_states s
+          ON e.state_fips = s.id
+        INNER JOIN counties c
+          ON e.county_fips = c.fips AND e.state_fips = c.us_state_id
+        LIMIT 10")
+  end
+
+  def self.getDetails(id)
+    @events = find_by_sql("SELECT e.id as id,
+          storm_type,
+          c.name as county_name, s.name as state_name,
+          begin_time, end_time, source,
+          e.forecast_office as forecast_office,
+            fo.name as forecast_office_name,
+            state_fo.name as forecast_office_state,
+          magnitude, magnitude_type,
+          property_damage, crop_damage,
+          narrative,
+          episode_id, ep.description as episode_narrative
+        FROM events e
+        INNER JOIN us_states s
+          ON e.id = #{id} AND e.state_fips = s.id
+        INNER JOIN counties c
+          ON e.county_fips = c.fips AND e.state_fips = c.us_state_id
+        INNER JOIN forecast_offices fo
+          ON e.forecast_office = fo.id
+        INNER JOIN us_states state_fo
+          ON fo.us_state_id = state_fo.id
+        INNER JOIN episodes ep
+          ON e.episode_id = ep.id")
+
+    @events.each do |event|
+      event.locations << Location.find_by_sql("
+          SELECT * FROM locations WHERE event_id = #{event.id}")
+      event.fatalities << Fatality.find_by_sql("
+          SELECT * FROM fatalities WHERE event_id = #{event.id}")
+    end
   end
 end
